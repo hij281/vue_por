4bda3fe73c0416f4b0e21a367e67af208ac699a1
hij <ijhwang@azsoft.kr>
2018-04-19 21:58:17 +0900

    인준
    프로젝트 설정

---
+++ vue_app/src/component/TodoHeader.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    <h1>Todo It</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+ h1{
+   color:#2FB52;
+   font-weight: 900;
+   margin: 2.5rem 0 1.5rem;
+ }
+</style>
